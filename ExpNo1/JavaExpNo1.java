95070daf470aa556a9dfc0345b082005b9774847
jegan-14 <109565296+jegan-14@users.noreply.github.com>
2022-08-18 15:30:34 +0530

    Add files via upload

---
+++ ExpNo1/JavaExpNo1.java
@@ -0,0 +1,15 @@
+
+package javaexpno1;
+public class JavaExpNo1 {
+    public static void main(String args[]) {
+        int a=Integer.valueOf(args[0]);
+        int b=Integer.valueOf(args[1]);
+       if(a>b) {
+            System.out.println(b);
+        }
+         else {
+            System.out.println(a);
+       }
+    }
+    
+}
