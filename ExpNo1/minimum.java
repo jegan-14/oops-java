0573b870ec8c9bea15b2832e59e0f0dc25c7a983
jegan-14 <109565296+jegan-14@users.noreply.github.com>
2022-08-18 20:04:34 +0530

    Add files via upload

---
+++ ExpNo1/minimum.java
@@ -0,0 +1,13 @@
+package javaexpno1;
+public class minimum 
+{
+    public static void main(String args[]) 
+    {
+        int a=Integer.valueOf(args[0]);
+        int b=Integer.valueOf(args[1]);
+       if(a>b) 
+            System.out.println(b+" is minimum");
+         else
+            System.out.println(a+" is minimum");
+}
+}
